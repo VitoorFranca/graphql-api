68c70e18877b489c0e5bf20b5915c0b8a305b849
VitoorFranca <franc.vitoor@gmail.com>
2022-02-07 16:13:28 -0300

    heroku deploy prepare

--- src/startServer.js
+++ src/startServer.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("apollo-server");
 module.exports = ({ resolvers, typeDefs }) => {
   const server = new ApolloServer({ resolvers, typeDefs });
   server
-    .listen()
+    .listen({ port: process.env.PORT || 4000 })
     .then(({ url }) => console.log("Servidor online na url: " + url))
     .catch((error) =>
       console.log("Não foi possivel inicializar oo servidor\nError: " + error)
